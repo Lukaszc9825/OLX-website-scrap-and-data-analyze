bc659d1815c0e840d2fccc5f8ed799cf1265e5f4
Łukasz Cieśluk <lukaszciesluk98@gmail.com>
2020-10-24 11:35:20 +0200

    make a class

--- scraper.py
+++ scraper.py
@@ -1,68 +1,78 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://www.olx.pl/elektronika/'
-
-headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-
-page = requests.get(url, headers=headers)
-
-# Convert to bs object
-soup = bs(page.content, 'html.parser')
-
-# Contains all informations about product
-product_info = {}
-products_list = []
-
-# Separate the products from each other 
-def separate(soupy):
-	# Save all off the products into a list
-	products = soupy.find_all(class_ = 'offer-wrapper')
+class Scraper(object):
 	
-	for product in products:
+	def __init__(self, url, headers):
+		self.url = url
+		self.headers = headers
+		self.page = requests.get(url, headers=headers)
+
+		# Convert to bs object
+		self.soup = bs(self.page.content, 'html.parser')
+
+		# Contains all informations about product
+		self.product_info = {}
+		self.products_list = []
+
+		Scraper.separate(self, self.soup)
+		Scraper.all_pages(self)
+
+		# Separate the products from each other 
+	def separate(self,soupy):
+		# Save all off the products into a list
+		self.products = soupy.find_all(class_ = 'offer-wrapper')
 		
-		# Separate single informations about product
-		info = product.find_all('td')
-		for i, data in enumerate(info):
-			#print(data.prettify())
-			if i == 0:
-				for temp in data.find_all('a'):
-					product_info['auction link'] = temp.get('href')
-				for temp in data.find_all('img'):
-					product_info['photo link'] = temp.get('src')
+		for product in self.products:
 			
-			elif i == 1:
-				product_info['auction name'] = data.find('strong').get_text()
-				product_info['category'] = data.find('small').get_text().strip()
-
-			elif i == 2:
-				product_info['price'] = data.find('strong').get_text()
-
-			elif i ==3:
-				for j, x in enumerate(data.find_all('span')):
-					if j == 0:
-						product_info['location'] = x.get_text()
-					else:
-						product_info['add time'] = x.get_text()
+			# Separate single informations about product
+			self.info = product.find_all('td')
+			for i, data in enumerate(self.info):
+				#print(data.prettify())
+				if i == 0:
+					for temp in data.find_all('a'):
+						self.product_info['auction link'] = temp.get('href')
+					for temp in data.find_all('img'):
+						self.product_info['photo link'] = temp.get('src')
 				
-		products_list.append(product_info.copy())			
-	return products_list
+				elif i == 1:
+					self.product_info['auction name'] = data.find('strong').get_text()
+					self.product_info['category'] = data.find('small').get_text().strip()
 
-def get_number_of_pages():
-	mess = soup.find_all(class_ = 'block br3 brc8 large tdnone lheight24')
-	for index, number in enumerate(mess):
-		if index == 12:
-			last = number.get_text().strip()
-	return int(last)		
+				elif i == 2:
+					self.product_info['price'] = data.find('strong').get_text()
 
-def all_pages():
-	for i in range(1, get_number_of_pages()):
-		url2 = 'https://www.olx.pl/elektronika/?page=' + str(i+1)
-		page2 = requests.get(url2, headers=headers)
-		soup2 = bs(page2.content, 'html.parser')
-		separate(soup2)	
+				elif i ==3:
+					for j, x in enumerate(data.find_all('span')):
+						if j == 0:
+							self.product_info['location'] = x.get_text()
+						else:
+							self.product_info['add time'] = x.get_text()
+					
+			self.products_list.append(self.product_info.copy())			
+		return self.products_list
 
-separate(soup)
-all_pages()
-print(len(products_list))
+	def get_number_of_pages(self):
+		self.mess = self.soup.find_all(class_ = 'block br3 brc8 large tdnone lheight24')
+		for index, number in enumerate(self.mess):
+			if index == 12:
+				self.last = number.get_text().strip()
+		return int(self.last)		
+
+	def all_pages(self):
+		for i in range(1, Scraper.get_number_of_pages(self)):
+			self.url2 = 'https://www.olx.pl/elektronika/?page=' + str(i+1)
+			self.page2 = requests.get(self.url2, headers=self.headers)
+			self.soup2 = bs(self.page2.content, 'html.parser')
+			Scraper.separate(self, self.soup2)
+
+
+if __name__ == '__main__':
+
+	url = 'https://www.olx.pl/elektronika/'
+
+	headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+	(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+
+	scraper = Scraper(url, headers)
+	print(scraper.products_list)
